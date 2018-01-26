3df92ae7f5acc251deb830ab6bf4aaa7e6016d92
Diana Thayer <garbados@gmail.com>
2018-01-26 08:33:34 -0800

    chore: update name in code

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { EventEmitter } = require('events')
 
 function log () {
   let msg = arguments[0]
-  arguments[0] = '[couch-aws-logs] ' + msg
+  arguments[0] = '[aws-couchwatch] ' + msg
   if (process.env.DEBUG || process.env.LOG) {
     console.log.apply(console, arguments)
   }
